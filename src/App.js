f4b503e301c0108ebf510b37c831ecd261bae64d
puranchandralohar <clpuran95@gmail.com>
2022-11-11 12:07:36 +0530

    Component Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./Components/Header/Header";
+import Index from "./Components/Main/Index";
 
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Index />
     </div>
   );
 }
